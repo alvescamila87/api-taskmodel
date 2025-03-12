063aa5102da187ae87a0eb98c6135c8e0b36f2ed
CAMILA ALVES <alvescamila87@gmail.com>
2025-03-12 20:52:56 -0300

    feat: application properties profile

---
+++ src/main/java/com/senai/taskmodel/user/entities/UserEntity.java
@@ -0,0 +1,18 @@
+package com.senai.taskmodel.user.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+}
