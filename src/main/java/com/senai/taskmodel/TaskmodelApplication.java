640f52d7346a34059355404e71d04d2a3f6ea66a
CAMILA ALVES <alvescamila87@gmail.com>
2025-03-12 20:25:44 -0300

    feat: start project

---
+++ src/main/java/com/senai/taskmodel/TaskmodelApplication.java
@@ -0,0 +1,13 @@
+package com.senai.taskmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmodelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/taskmodel/TaskmodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.taskmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
