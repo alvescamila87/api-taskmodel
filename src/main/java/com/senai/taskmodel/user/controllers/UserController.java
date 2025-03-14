d905b2381ad474f64e7b57ecf2b9a1e0540be3e7
CAMILA ALVES <alvescamila87@gmail.com>
2025-03-13 21:32:26 -0300

    feat: create user controller

--- src/main/java/com/senai/taskmodel/user/controllers/UserController.java
+++ src/main/java/com/senai/taskmodel/user/controllers/UserController.java
@@ -2,10 +2,9 @@ package com.senai.taskmodel.user.controllers;
 
 import com.senai.taskmodel.user.dtos.UserDTO;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,5 +18,26 @@ public class UserController {
         List<UserDTO> listUsers = new ArrayList<>();
 
         return ResponseEntity.ok().body(listUsers);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@RequestBody @Valid UserDTO userDTO) {
+        UserDTO newUser = new UserDTO();
+
+        return ResponseEntity.ok().body(newUser);
+    }
+
+    @PutMapping("/{email}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable String email, @RequestBody @Valid UserDTO userDTO) {
+        UserDTO updateUser = new UserDTO();
+
+        return ResponseEntity.ok().body(updateUser);
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<UserDTO> deleteUser(@PathVariable String email) {
+        UserDTO deleteUser = new UserDTO();
+
+        return ResponseEntity.status(204).body(deleteUser);
     }
 }
