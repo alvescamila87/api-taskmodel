9a21f677e1879a7d477d354e8478f07a26541af0
Camila <alvescamila87@gmail.com>
2025-03-23 18:27:29 -0300

    feat: update user controller

--- src/test/java/com/senai/taskmodel/user/controllers/UserControllerTest.java
+++ src/test/java/com/senai/taskmodel/user/controllers/UserControllerTest.java
@@ -1,6 +1,7 @@
 package com.senai.taskmodel.user.controllers;
 
 import com.senai.taskmodel.user.dtos.ResponseUserDTO;
+import com.senai.taskmodel.user.dtos.UserDTO;
 import com.senai.taskmodel.user.services.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -12,9 +13,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @WebMvcTest(UserControllerTest.class)
@@ -95,5 +97,132 @@ public class UserControllerTest {
 
         assertEquals(HttpStatus.NOT_FOUND, responseDTO.getStatusCode());
         assertFalse(responseDTO.getBody().getSuccess());
+    }
+
+    @Test
+    void when_create_user_with_success_email() {
+        ResponseUserDTO responseUserDTO = ResponseUserDTO
+                .builder()
+                .name(USER_DEFAULT_NAME)
+                .email(USER_DEFAULT_EMAIL)
+                .success(true)
+                .build();
+
+        when(service.getUserByEmail(USER_DEFAULT_EMAIL)).thenReturn(responseUserDTO);
+
+        when(service.createUser(any(UserDTO.class))).thenReturn(responseUserDTO);
+
+        UserDTO userDTO = UserDTO
+                .builder()
+                .name(USER_DEFAULT_NAME)
+                .email(USER_DEFAULT_EMAIL)
+                .build();
+
+        ResponseEntity<ResponseUserDTO> newUser = controller.createUser(userDTO);
+
+        assertEquals(HttpStatus.CREATED, newUser.getStatusCode());
+        assertTrue(newUser.getBody().getSuccess());
+    }
+
+    @Test
+    void when_create_user_with_existing_email() {
+        ResponseUserDTO responseUserDTO = ResponseUserDTO
+                .builder()
+                .name(USER_DEFAULT_NAME)
+                .email(USER_DEFAULT_EMAIL)
+                .success(false)
+                .build();
+
+        when(service.getUserByEmail(USER_DEFAULT_EMAIL)).thenReturn(responseUserDTO);
+
+        when(service.createUser(any(UserDTO.class))).thenReturn(responseUserDTO);
+
+        UserDTO userDTO = UserDTO
+                .builder()
+                .name(USER_DEFAULT_NAME)
+                .email(USER_DEFAULT_EMAIL)
+                .build();
+
+        ResponseEntity<ResponseUserDTO> newUser = controller.createUser(userDTO);
+
+        assertEquals(HttpStatus.CONFLICT, newUser.getStatusCode());
+        assertFalse(newUser.getBody().getSuccess());
+    }
+
+    @Test
+    void when_update_user_with_success_email() {
+        ResponseUserDTO responseUserDTO = ResponseUserDTO
+                .builder()
+                .name(USER_DEFAULT_NAME)
+                .email(USER_DEFAULT_EMAIL)
+                .success(true)
+                .build();
+
+        when(service.getUserByEmail(USER_DEFAULT_EMAIL)).thenReturn(responseUserDTO);
+
+        UserDTO userDTO = UserDTO
+                .builder()
+                .name(USER_DEFAULT_NAME)
+                .email(USER_DEFAULT_EMAIL)
+                .build();
+
+        when(service.updateUser(eq(USER_DEFAULT_EMAIL), any(UserDTO.class))).thenReturn(responseUserDTO);
+
+        ResponseEntity<ResponseUserDTO> updateUser = controller.updateUser(USER_DEFAULT_EMAIL, userDTO);
+
+        assertEquals(HttpStatus.OK, updateUser.getStatusCode());
+        assertEquals(USER_DEFAULT_NAME, updateUser.getBody().getName());
+        assertEquals(USER_DEFAULT_EMAIL, updateUser.getBody().getEmail());
+        assertTrue(updateUser.getBody().getSuccess());
+    }
+
+    @Test
+    void when_update_user_with_wrong_email() {
+        ResponseUserDTO responseUserDTO = ResponseUserDTO
+                .builder()
+                .name(USER_DEFAULT_NAME)
+                .email("not_exist_email@gmail.com")
+                .success(false)
+                .build();
+
+        when(service.getUserByEmail("not_exist_email@gmail.com")).thenReturn(responseUserDTO);
+
+        when(service.updateUser(eq("not_exist_email@gmail.com"), any(UserDTO.class))).thenReturn(responseUserDTO);
+
+        UserDTO userDTO = UserDTO
+                .builder()
+                .name(USER_DEFAULT_NAME)
+                .email("not_exist_email@gmail.com")
+                .build();
+
+        ResponseEntity<ResponseUserDTO> newUser = controller.updateUser("not_exist_email@gmail.com", userDTO);
+
+        assertEquals(HttpStatus.NOT_FOUND, newUser.getStatusCode());
+        assertFalse(newUser.getBody().getSuccess());
+    }
+
+    @Test
+    void when_update_user_with_the_same_email_in_use() {
+        ResponseUserDTO responseUserDTO = ResponseUserDTO
+                .builder()
+                .name(USER_DEFAULT_NAME)
+                .email("not_exist_email@gmail.com")
+                .success(false)
+                .build();
+
+        when(service.getUserByEmail("not_exist_email@gmail.com")).thenReturn(responseUserDTO);
+
+        when(service.updateUser(eq("not_exist_email@gmail.com"), any(UserDTO.class))).thenReturn(responseUserDTO);
+
+        UserDTO userDTO = UserDTO
+                .builder()
+                .name(USER_DEFAULT_NAME)
+                .email("not_exist_email@gmail.com")
+                .build();
+
+        ResponseEntity<ResponseUserDTO> newUser = controller.updateUser("not_exist_email@gmail.com", userDTO);
+
+        assertEquals(HttpStatus.NOT_FOUND, newUser.getStatusCode());
+        assertFalse(newUser.getBody().getSuccess());
     }
 }
