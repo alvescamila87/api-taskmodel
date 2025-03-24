628943acb569c67fed1ea4fa05e74b749af24db4
CAMILA ALVES <alvescamila87@gmail.com>
2025-03-24 20:49:16 -0300

    feat: create task

--- src/test/java/com/senai/taskmodel/task/services/TaskServiceTest.java
+++ src/test/java/com/senai/taskmodel/task/services/TaskServiceTest.java
@@ -1,9 +1,11 @@
 package com.senai.taskmodel.task.services;
 
 import com.senai.taskmodel.task.dtos.ResponseTaskDTO;
+import com.senai.taskmodel.task.dtos.TaskDTO;
 import com.senai.taskmodel.task.entities.TaskEntity;
 import com.senai.taskmodel.task.enums.EnumStatus;
 import com.senai.taskmodel.task.repositories.TaskRepository;
+import com.senai.taskmodel.user.dtos.UserDTO;
 import com.senai.taskmodel.user.entities.UserEntity;
 import com.senai.taskmodel.user.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -16,9 +18,9 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -79,4 +81,73 @@ public class TaskServiceTest {
         verify(repository, times(1)).findAll();
 
     }
+
+    @Test
+    void when_find_tasks_not_exists_then_return_empty_list() {
+        List<TaskEntity> taskEntityList = new ArrayList<>();
+
+        when(repository.findAll()).thenReturn(taskEntityList);
+
+        List<ResponseTaskDTO> responseTaskDTOList = service.findAllTasks();
+
+        assertNotNull(responseTaskDTOList);
+        assertEquals(0, responseTaskDTOList.size());
+
+        verify(repository, times(1)).findAll();
+
+    }
+
+    @Test
+    void when_create_task_then_return_task_success() {
+        UserEntity userEntity = UserEntity
+                .builder()
+                .name(TASK_DEFAULT_NAME)
+                .email(TASK_DEFAULT_USER_EMAIL)
+                .build();
+
+        when(userRepository.save(any(UserEntity.class))).thenReturn(userEntity);
+
+        TaskDTO newTaskDTO = TaskDTO
+                .builder()
+                .title(TASK_DEFAULT_TITLE)
+                .description(TASK_DEFAULT_DESCRIPTION)
+                .status(TASK_DEFAULT_STATUS)
+                .dateTask(TASK_DEFAULT_TASK_DATE)
+                .userEmail(userEntity.getEmail())
+                .build();
+
+        when(userRepository.findByEmail(newTaskDTO.getUserEmail())).thenReturn(Optional.of(userEntity));
+
+        TaskEntity newTaskEntity = TaskEntity
+                .builder()
+                .title(TASK_DEFAULT_TITLE)
+                .description(TASK_DEFAULT_DESCRIPTION)
+                .status(TASK_DEFAULT_STATUS)
+                .dateTask(TASK_DEFAULT_TASK_DATE)
+                .user(userEntity)
+                .build();
+
+        when(repository.save(any(TaskEntity.class))).thenReturn(newTaskEntity);
+
+        ResponseTaskDTO newTaskResponseDTO = service.createTask(newTaskDTO);
+
+        assertNotNull(newTaskResponseDTO);
+        assertEquals(TASK_DEFAULT_ID, newTaskResponseDTO.getId());
+        assertEquals(TASK_DEFAULT_TITLE, newTaskResponseDTO.getTitle());
+        assertEquals(TASK_DEFAULT_DESCRIPTION, newTaskResponseDTO.getDescription());
+        assertEquals(TASK_DEFAULT_STATUS, newTaskResponseDTO.getStatus());
+        assertEquals(TASK_DEFAULT_TASK_DATE, newTaskResponseDTO.getDateTask());
+        assertEquals(TASK_DEFAULT_USER_EMAIL, userEntity.getEmail());
+        assertEquals("Task has been created", newTaskResponseDTO.getMensagem());
+        assertTrue(newTaskResponseDTO.getSuccess());
+
+        //verify(repository, times(1)).save(any(TaskEntity.class));
+        //verify(userRepository, times(1)).findByEmail(TASK_DEFAULT_USER_EMAIL);
+        verify(service, times(1)).createTask(newTaskDTO);
+
+
+
+
+    }
+
 }
