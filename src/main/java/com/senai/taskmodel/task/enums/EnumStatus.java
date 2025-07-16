88f9ccc7ff1b8fd7e9bb3b0f4cea91e877ab1aa5
Camila <alvescamila87@gmail.com>
2025-07-15 21:20:51 -0300

    enum

--- src/main/java/com/senai/taskmodel/task/enums/EnumStatus.java
+++ src/main/java/com/senai/taskmodel/task/enums/EnumStatus.java
@@ -1,8 +1,22 @@
 package com.senai.taskmodel.task.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;@
 public enum EnumStatus {
-    OPEN,
-    IN_PROGRESS,
-    CONCLUDED,
-    CANCELLED
+    OPEN("Open"),
+    IN_PROGRESS("In progress"),
+    CONCLUDED("Concluded"),
+    CANCELLED("Cancelled");
+
+   private final String label;
+
+   // Contrutor
+    EnumStatus (String label) {
+        this.label = label;
+    }
+
+    @JsonValue
+    public String getLabel() {
+        return label;
+    }
+
 }
